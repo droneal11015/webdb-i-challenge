198cc4984c6bd029ed153682c901bba033fe4d11
droneal11015 <droneal11015@gmail.com>
2019-06-10 18:35:52 -0400

    working on CRUD for end points

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 const express = require('express');
+const morgan = require('morgan');
+const db = require('./data/accounts-model');
 
 const server = express();
+
+server.use(express.json());
+server.use(morgan('dev'));
+
+server.get('/', async(req, res) => {
+    try {
+        const allAccounts = await db.find(req.body);
+        if (allAccounts.length > 0){
+            res.json(allAccounts);
+        } else {
+            res.status(400).json({message: "No accounts found."}.message)
+        }
+    } catch (err){
+        res.status(500).json(err.message);
+    }
+})
+
+server.get('/:id', async(req, res) => {
+    try{
+        const account = await db.findById(req.params.id);
+        res.json(account);
+    } catch (err){
+        res.status(500).json(err.message);
+    }
+})
+
 
 // your code here
 
